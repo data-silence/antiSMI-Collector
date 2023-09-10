854e867d4655477ea0e2f4d57a78b5cc1f2f502d
max-science <enjoy@data-silence.xyz>
2023-09-10 22:50:15 +0300

    refactoring names and descriptions

--- main.py
+++ main.py
@@ -46,22 +46,22 @@ def fixing():
 if __name__ == '__main__':
     try:
         # This module serves for debugging the work of individual modules
-        shopping()
-        cooking()
-        serving()
+        # shopping()
+        # cooking()
+        # serving()
         # fixing()
 
-        # scheduler = BlockingScheduler()
-        # scheduler.configure(timezone='Europe/Moscow')
-        # scheduler.add_job(fixing, 'cron', hour=0, minute=0, id='fixer',
-        #                   max_instances=10, misfire_grace_time=600)
-        # scheduler.add_job(shopping, 'cron', hour='6-21, 23', minute=55, second=10, id='shopper',
-        #                   max_instances=10, misfire_grace_time=600)
-        # scheduler.add_job(cooking, 'cron', hour='0, 7-21', id='cooker',
-        #                   max_instances=10, misfire_grace_time=600)
-        # scheduler.add_job(serving, 'cron', hour='9-21/4, 23', minute=55, id='server',
-        #                   max_instances=10, misfire_grace_time=600)
-        # scheduler.start()
+        scheduler = BlockingScheduler()
+        scheduler.configure(timezone='Europe/Moscow')
+        scheduler.add_job(fixing, 'cron', hour=0, minute=0, id='fixer',
+                          max_instances=10, misfire_grace_time=600)
+        scheduler.add_job(shopping, 'cron', hour='6-21, 23', minute=55, second=10, id='shopper',
+                          max_instances=10, misfire_grace_time=600)
+        scheduler.add_job(cooking, 'cron', hour='0, 7-21', id='cooker',
+                          max_instances=10, misfire_grace_time=600)
+        scheduler.add_job(serving, 'cron', hour='9-21/4, 23', minute=55, id='server',
+                          max_instances=10, misfire_grace_time=600)
+        scheduler.start()
 
     except Exception as e:
         logger.exception(e)
